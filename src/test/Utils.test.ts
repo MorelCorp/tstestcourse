4972eaec2c89a7838825242e325d441975314795
MorelCorpJeff <jfmorel@gmail.com>
2024-05-08 00:21:24 +0000

    section 2

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,9 +1,55 @@
-import { toUpperCase } from "../app/Utils"
+import { getStringInfo, toUpperCase } from "../app/Utils"
 
 describe ('Utils test suite', ()=>{
     
-    test('should return uppercase', ()=>{
-        const result = toUpperCase('abc');
-        expect(result).toBe('ABC');
+    it('should return uppercase of a valid string', ()=>{
+        // arrange:
+        const sut = toUpperCase;
+        const expected = 'ABC'
+        
+        // act:
+        const actual = sut('abc');
+
+        // assert:
+        expect(actual).toBe(expected);
     })
-})
+
+    describe('ToUpperCase examples', ()=>{
+        it.each([
+            {input:'abc', expected:'ABC'},
+            {input:'My-String', expected:'MY-STRING'},
+            {input:'def', expected:'DEF'},
+        ])('$input toUpperCase should be $expected', ({input, expected})=>{
+            const actual = toUpperCase(input);
+            expect(actual).toBe(expected);
+        });
+    })
+
+
+    describe('getStringInfo for arg My-String should', ()=>{
+        test('return right length', ()=>{
+            const actual = getStringInfo('My-String');
+            expect(actual.characters).toHaveLength(9);
+        });
+        test('return right lower case', ()=>{
+            const actual = getStringInfo('My-String');
+            expect(actual.lowerCase).toBe('my-string');
+        });
+        test('return right upper case', ()=>{
+            const actual = getStringInfo('My-String');
+            expect(actual.upperCase).toBe('MY-STRING');
+        });        
+        test('return right characters', ()=>{
+            const actual = getStringInfo('My-String');
+            expect(actual.characters).toContain<string>('M');
+        });
+        test('return defined extra info', ()=>{
+            const actual = getStringInfo('My-String');
+            expect(actual.extraInfo).toBeDefined();
+            expect(actual.extraInfo).toBeTruthy();
+        });
+    })
+
+
+
+});
